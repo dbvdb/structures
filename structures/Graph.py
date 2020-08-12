baf555ee1644b67c09894937945488a9a2239093
danialoyster <danial.bidekani@gmail.com>
2020-08-12 23:09:09 +0430

    Add add_attribute() method and comments

--- structures/Graph.py
+++ structures/Graph.py
@@ -12,27 +12,37 @@ class Graph:
         self.nodes = {}
 
     def add_node(self, value: int):
+        """Add a new node to the graph"""
         node = Node()
 
         self.nodes[value] = node
 
     def add_edge(self, src, dest):
+        """Add an edge to a node"""
         self.nodes[src].neighbours.add(dest)
         self.nodes[dest].neighbours.add(src)
 
     def add_edge_list(self, lst: list):
+        """Add a list of edges to the graph"""
         for src, dest in lst:
             self.add_edge(src, dest)
 
+    def add_attribute(self, value, attr: dict) -> None:
+        """Add new attributes to a vertex"""
+        for k, v in attr.items():
+            self.nodes[value].attributes[k] = v
+
     def adjacency_list(self, value):
+        """Create an iterator of neighbours"""
         for node in self.nodes[value].neighbours:
             yield (value, node)
 
     def degree(self, value):
+        """Compute the degree for a node"""
         return len(self.nodes[value].neighbours)
 
     def __repr__(self):
-        return f"<Graph "
+        return f"<Graph>"
 
 
 if __name__ == '__main__':
@@ -51,6 +61,8 @@ if __name__ == '__main__':
 
     print(graph.degree(3))
 
+    graph.add_attribute(1, {"age": 12, "city": "Tehran"})
     for i in graph.adjacency_list(1):
         print(i)
 
+    print(graph.nodes[1])
