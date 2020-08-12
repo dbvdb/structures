1803a7557de1634ab25597c5106894cf1234319e
danialoyster <danial.bidekani@gmail.com>
2020-08-12 23:26:59 +0430

    Add Tree.py

---
+++ structures/Tree.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value=0, children=None):
+        self.value = value
+        self.children = children
+
+
+class Tree:
+    def __init__(self):
+        self.root
+        self.children
+
+    def in_order(self):
+        ...
+
+    def pre_order(self):
+        ...
+
+    def post_order(self):
+        ...
